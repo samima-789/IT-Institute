4a005107fe0733839120dcf41a9a7fcde46e3023
It-Institute <tithysamima789@gmail.com>
2024-02-22 00:02:58 +0600

    Convert all CDN link to locally files.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ----- SEARCHBAR & ACCOUNT ----- //
 let btnsearch = document.querySelector("#search")
 let btnsearch2 = document.querySelector("#search1")
 let searchbar = document.querySelector(".search-bar")
@@ -40,17 +41,103 @@ let bars = document.querySelector("fa-solid fa-bars")
 //bars.addEventListener("click",function(){
    // menu.classList.toggle("show_menu")
 //})
-var $grid = $('.items').isotope({
-    
-});
 
+// ----- HERO ---- //
+$(document).ready(function(){
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+        const progressContent = document.querySelector(".autoplay-progress span");
+        var swiper = new Swiper(".mySwiper", {
+          spaceBetween: 50,
+          centeredSlides: true,
+          autoplay: {
+            delay: 6000,
+            disableOnInteraction: false
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          },
+          on: {
+            autoplayTimeLeft(s, time, progress) {
+              progressCircle.style.setProperty("--progress", 1 - progress);
+              progressContent.textContent = `${Math.ceil(time / 1500)}s`;
+            }
+          }
+        });
+
+        $(".counter").counterUp({
+             delay: 10,
+             time: 1200
+        });
+
+
+    var swiper = new Swiper('.swiper', {
+        spaceBetween: 50,
+          centeredSlides: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  });
+
+
+// ----- ISOTOP ----- //
+var $grid = $('.items').isotope({   
+});
 $('.menu').on( 'click', 'li', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
-
 $('.menu').on( 'click', 'li', function() {
   $(this).activeClass('active').siblings.removeClass('active');
+});
 
+
+// ----- REVIEW ----- //
+  $(document).ready(function(){
+  $('.slider-item').slick({
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 });
 
