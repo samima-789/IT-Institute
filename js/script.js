72fc85be40a6bb28c8005aaef737204a52678546
It-Institute <tithysamima789@gmail.com>
2024-02-23 00:35:06 +0600

    Updated navigation bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // ----- SEARCHBAR & ACCOUNT ----- //
 let btnsearch = document.querySelector("#search")
-let btnsearch2 = document.querySelector("#search1")
+//let btnsearch2 = document.querySelector("search1")
 let searchbar = document.querySelector(".search-bar")
 let close = document.querySelector("#close")
 
@@ -9,9 +9,9 @@ btnsearch.addEventListener("click",function(){
     searchbar.classList.toggle("show_searchbar")
 })
 
-btnsearch2.addEventListener("click",function(){
-    searchbar.classList.toggle("show_searchbar")
-})
+// btnsearch2.addEventListener("click",function(){
+//   searchbar.classList.toggle("show_searchbar")
+// })
 
 close.addEventListener("click",function(){
     searchbar.classList.toggle("show_searchbar")
@@ -19,28 +19,27 @@ close.addEventListener("click",function(){
 
 
 let btn_form =document.querySelector("#btn_form")
-let btn_form2 =document.querySelector("#btn_form2")
+//let btn_form2 =document.querySelector("#btn_form2")
 let form =document.querySelector(".form")
 let close_form =document.querySelector("#close_form")
 
 btn_form.addEventListener("click",function(){
     form.classList.toggle("show_form")
 })
-
-btn_form2.addEventListener("click",function(){
-    form.classList.toggle("show_form")
-})
-
+// btn_form2.addEventListener("click",function(){
+//   form.classList.toggle("show_form")
+// })
 close_form.addEventListener("click",function(){
     form.classList.toggle("show_form")
 })
 
-let menu = document.querySelector("ul")
-let bars = document.querySelector("fa-solid fa-bars")
 
-//bars.addEventListener("click",function(){
-   // menu.classList.toggle("show_menu")
-//})
+let menu = document.querySelector("ul")
+let bars = document.querySelector(".fa-bars-staggered")
+
+bars.addEventListener("click",function(){
+  menu.classList.toggle("show_menu")
+})
 
 // ----- HERO ---- //
 $(document).ready(function(){
